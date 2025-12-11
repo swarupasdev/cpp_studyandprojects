f0c8892f9dd2ef38b9ccacbfcf0276c0b91cb038
Swarup143 <santiswarupnayak1@gmail.com>
2025-12-11 12:29:03 +0530

    Initial commit of Project1

---
+++ Log.h
@@ -0,0 +1,5 @@
+#pragma once
+void Log(const char* message);
+void initLog();
+// as you can see I have created a header file which can include both functions 
+//now we will  use it as #include "Log.h" in both cpp file to link these 2 programe
