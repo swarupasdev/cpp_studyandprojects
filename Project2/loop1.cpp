295fd072efafb0c021bff342e13168275f894c35
Santiswarup Nayak <122181079+swarupasdev@users.noreply.github.com>
2025-12-15 23:17:47 +0530

    Add files via upload

---
+++ Project2/loop1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// if I want to print Hello world 5 times then 
+	cout << "hello world" << endl;
+	cout << "hello world" << endl;
+	cout << "hello world" << endl;
+	cout << "hello world" << endl;
+	cout << "hello world" << endl;
+
+	// If  want to print a lot of times using output function again n again is not efficient
+	// That is why there is looping 
+
+
+	return 0;
+
+}
