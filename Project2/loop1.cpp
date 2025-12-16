2d60f6125e570d033bd078eb54c47f537a22fb8a
Santiswarup Nayak <122181079+swarupasdev@users.noreply.github.com>
2025-12-16 23:07:12 +0530

    Implement loop to print 'hello world' 5 times
    Added a loop to print 'hello world' 5 times instead of repeating print statements.

--- Project2/loop1.cpp
+++ Project2/loop1.cpp
@@ -4,16 +4,20 @@ using namespace std;
 int main()
 {
 	// if I want to print Hello world 5 times then 
+	/*
 	cout << "hello world" << endl;
 	cout << "hello world" << endl;
 	cout << "hello world" << endl;
 	cout << "hello world" << endl;
 	cout << "hello world" << endl;
-
+	*/
 	// If  want to print a lot of times using output function again n again is not efficient
 	// That is why there is looping 
-
-
+	for (int i = 0; i < 5; i++)		//initialized as i= 0 while the loop is going to run 0 to less than 5 means 0 to 4 is 5 										times and i++ will further carry this loop 
+{
+	cout << "hello world" << endl;
+}
 	return 0;
 
 }
+
